123558691a8dff40ee43f54c98ab3061851e25f8
youshitsune <luka@youshitsune.me>
2023-04-06 16:06:49 +0200

    Implement death rate

--- main.py
+++ main.py
@@ -3,18 +3,24 @@ import pandas as pd
 
 st.write("# Virus simulator")
 
-a = st.slider("How much people can one person infect?", min_value=0, max_value=10)
-b = st.slider("For how many days?", min_value=2, max_value=20)
-
-if a == 0:
-    st.write("Choose a number")
+infect_rate = st.slider("How much people can one person infect?", min_value=0, max_value=10)
+days = st.slider("For how many days?", min_value=2, max_value=20)
+death_rate = st.slider("What is death rate?", min_value=0, max_value=100)
+if infect_rate == 0:
+    st.write("Choose an infect rate")
 else:
     number_infection = [1]
+    dead = [0]
     infected = 1
-    for i in range(1,b):
-        infected = infected + infected*a
+    for i in range(1,days):
+        infected = infected + infected*infect_rate
         number_infection.append(infected)
-    
-    tbl = pd.DataFrame(data=number_infection,columns=["Infected people"],index=[x for x in range(1, b+1)])
+        if (death_rate/100)*sum(number_infection) < 1:
+            dead.append(0)
+        else:
+            number_infection[-1] -= int((death_rate/100)*sum(number_infection))
+            dead.append(int((death_rate/100)*sum(number_infection)))
+
+    tbl = pd.DataFrame(data={"Infected people": number_infection, "Deaths": dead}, index=[x for x in range(0, days)])
     st.table(tbl)
     st.line_chart(tbl)
